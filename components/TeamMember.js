f011ac855c712db10009639043c32d519ba7f5ba
saathvikdm <saathvikdm@outlook.com>
2021-12-06 00:22:10 +0530

    refactoring wip

---
+++ components/TeamMember.js
@@ -0,0 +1,42 @@
+import { teamData } from "../data-store/team.js";
+
+const TeamMember = ({ picture, name, position, linkedIn }) => {
+  return `
+    <div class="col-6 col-md-4 col-lg-3">
+        <div class="team-card py-4">
+            <div class="ratio ratio-4x3">
+                <img src="${picture}" alt="team member" />
+            </div>
+            <div class="team-title">
+                <h5>${name}</h5>
+                <span>${position}</span>
+                ${
+                  linkedIn
+                    ? `<a href="${linkedIn}"><i class="team-icon fab fa-linkedin-in"></i></a>`
+                    : ``
+                }
+            </div>
+        </div>
+    </div>`;
+};
+
+const teamContainer = document.getElementById("team-container");
+
+if (teamContainer) {
+  teamContainer.innerHTML = teamData
+    .map(({ year, team }, i) => {
+      return `
+      <div class="container teamBlock top-block">
+        <h1>Our Team ${year}</h1>
+        <hr />
+        <div class="row justify-content-center">
+            ${team
+              .map((teamMember, i) => {
+                return TeamMember(teamMember);
+              })
+              .join("")}
+        </div>
+      </div>`;
+    })
+    .join("");
+}
---
+++ data-store/team.js
@@ -0,0 +1,213 @@
+export const teamData = [
+  {
+    year: "2021-2022",
+    team: [
+      {
+        name: "Dr. Gururaj H L",
+        position: "Faculty Sponsor",
+        picture: "./Assets/Crop/Gururaj.png",
+        linkedIn: "https://in.linkedin.com/in/gururaj-h-l-92513539",
+      },
+      {
+        name: "Prof. Vidyashree K P",
+        position: "Web Sponsor",
+        picture: "./Assets/Crop/2021/VKP.jpg",
+      },
+      {
+        name: "Saathvik D M",
+        position: "Chair",
+        picture: "./Assets/Crop/2021/saathvik.jpg",
+        linkedIn: "https://www.linkedin.com/in/saathvikdm/",
+      },
+      {
+        name: "Nydile",
+        position: "Vice-Chair",
+        picture: "./Assets/Crop/2021/nydile.jpg",
+        linkedIn: "https://www.linkedin.com/in/nydile-gowda-0b37a7204",
+      },
+      {
+        name: "Maanyatha M",
+        position: "Secretary",
+        picture: "./Assets/Crop/2021/maanyatha.jpg",
+        linkedIn: "https://www.linkedin.com/in/maanyatha-mahesh-255094216",
+      },
+      {
+        name: "Madhurya M J",
+        position: "Treasurer",
+        picture: "./Assets/Crop/2021/madhurya.jpg",
+        linkedIn: "https://www.linkedin.com/in/madhuryamj/",
+      },
+      {
+        name: "Suhas G",
+        position: "Web Master",
+        picture: "./Assets/Crop/2021/suhas.jpg",
+        linkedIn: "https://www.linkedin.com/in/suhas-g-4a5662219",
+      },
+      {
+        name: "Mohammed Kaif Khan Ghori",
+        position: "Technical Head",
+        picture: "./Assets/Crop/2021/Kaif.jpg",
+        linkedIn:
+          "https://www.linkedin.com/in/mohammed-kaif-khan-ghori-73a488217",
+      },
+    ],
+  },
+  {
+    year: "2020 - 2021",
+    team: [
+      {
+        name: "Dr. Gururaj H L",
+        position: "Faculty Sponsor",
+        picture: "./Assets/Crop/Gururaj.png",
+        linkedIn: "https://in.linkedin.com/in/gururaj-h-l-92513539",
+      },
+      {
+        name: "Neha R",
+        position: "Chair",
+        picture: "./Assets/Crop/Neha.png",
+        linkedIn: "https://www.linkedin.com/in/neha-r-8288b6173",
+      },
+      {
+        name: "Reena Reddy",
+        position: "Vice-Chair",
+        picture: "./Assets/Crop/Reena.png",
+      },
+      {
+        name: "Suraj G",
+        position: "Secretary",
+        picture: "./Assets/Crop/SurajG.png",
+        linkedIn: "https://www.linkedin.com/in/suraj-g-5863b0113",
+      },
+      {
+        name: "Sathya Pramod",
+        position: "Treasurer",
+        picture: "./Assets/Crop/Pramod.png",
+      },
+      {
+        name: "Nithin B O",
+        position: "Web Master",
+        picture: "./Assets/Crop/Nithin.png",
+        linkedIn: "https://www.linkedin.com/in/nithin-b-o-1b601816b/",
+      },
+      {
+        name: "Saathvik D M",
+        position: "Web Master",
+        picture: "./Assets/Crop/Saathvik.png",
+        linkedIn: "https://www.linkedin.com/in/saathvikdm/",
+      },
+      {
+        name: "Vishal B",
+        position: "Event Co-Ordinator",
+        picture: "./Assets/Crop/Vishal.png",
+        linkedIn: "https://www.linkedin.com/in/vishal-b-8158871a1/",
+      },
+      {
+        name: "Sharmila",
+        position: "Event Co-Ordinator",
+        picture: "./Assets/Crop/Sharmila.png",
+      },
+      {
+        name: "Shrivatsa G",
+        position: "Event Co-Ordinator",
+        picture: "./Assets/Crop/Sri.png",
+      },
+    ],
+  },
+  {
+    year: "2019 - 2020",
+    team: [
+      {
+        name: "Dr. Gururaj H L",
+        position: "Faculty Sponsor",
+        picture: "./Assets/Crop/Gururaj.png",
+        linkedIn: "https://in.linkedin.com/in/gururaj-h-l-92513539",
+      },
+      {
+        name: "Manoj Athreya",
+        position: "Chair",
+        picture: "./Assets/Crop/2019/Manoj.png",
+        linkedIn: "https://www.linkedin.com/in/manojathreyaa",
+      },
+      {
+        name: "Neha R",
+        position: "Vice-Chair",
+        picture: "./Assets/Crop/Neha.png",
+        linkedIn: "https://www.linkedin.com/in/neha-r-8288b6173",
+      },
+      {
+        name: "Reena Reddy",
+        position: "Secretary",
+        picture: "./Assets/Crop/Reena.png",
+      },
+      {
+        name: "Ashwin A Kumar",
+        position: "Treasurer",
+        picture: "./Assets/Crop/2019/Ashwin.png",
+        linkedIn: "https://www.linkedin.com/in/ashwin-a-k/",
+      },
+      {
+        name: "Parikshith H",
+        position: "Web Master",
+        picture: "./Assets/Crop/2019/Parikshith.png",
+        linkedIn: "https://www.linkedin.com/in/parikshithh/",
+      },
+      {
+        name: "Ismael R Nadaf",
+        position: "Head Creative & Design",
+        picture: "Assets/Crop/2019/ismael.png",
+      },
+      {
+        name: "Sinchana C",
+        position: "Event Convener",
+        picture: "./Assets/Crop/2019/Sinchana.png",
+        linkedIn: "https://www.linkedin.com/in/sinchana-chandan-40216015a",
+      },
+      {
+        name: "Vijay Kumar",
+        position: "Publicity and Outreach Co-Ordinator",
+        picture: "./Assets/Crop/2019/Vijay.png",
+      },
+    ],
+  },
+  {
+    year: "2018 - 2019",
+    team: [
+      {
+        name: "Dr. Gururaj H L",
+        position: "Faculty Sponsor",
+        picture: "./Assets/Crop/Gururaj.png",
+        linkedIn: "https://in.linkedin.com/in/gururaj-h-l-92513539",
+      },
+      {
+        name: "Keerthana M C",
+        position: "Chair",
+        picture: "./Assets/Crop/2018/keerthana.png",
+        linkedIn: "https://www.linkedin.com/in/keerthana-m-c-509479148/",
+      },
+      {
+        name: "Shashank Athreya",
+        position: "Vice-Chair",
+        picture: "./Assets/Crop/2018/shashank.png",
+        linkedIn: "https://www.linkedin.com/in/shashankathreya/",
+      },
+      {
+        name: "Shreyas Dixit",
+        position: "Secretary",
+        picture: "./Assets/Crop/2018/ShreyasD.png",
+        linkedIn: "https://www.linkedin.com/in/shreyas-dixit/",
+      },
+      {
+        name: "Madhurish Katta",
+        position: "Treasurer",
+        picture: "./Assets/Crop/2018/Madhurish.png",
+        linkedIn: "https://www.linkedin.com/in/madhurish/",
+      },
+      {
+        name: "Shreyas V",
+        position: "Web Master",
+        picture: "./Assets/Crop/2018/Shreyas.jpg",
+        linkedIn: "https://www.linkedin.com/in/shreyasvenkatesh06/",
+      },
+    ],
+  },
+];
