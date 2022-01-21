ccd50b56a1465e336e280270f96ed8a4fca1a663
suhasgirish <20cse0080@vvce.ac.in>
2022-01-21 14:03:49 +0530

    Update

---
+++ data-store/events.js
@@ -0,0 +1,166 @@
+export const eventData = [
+    {
+        title : "Hour of Code 2021-22"
+        desc : " VVCE ACM Student Chapter conducted Hour of Code in three Government Schools K Puttaswamy School, Bogadi Govt. School and M G Koppal Govt School."
+        date : "15th December, 2021"
+        poster : "./Assets/Kp.jpg"
+    }
+    {
+        title : "Induction to VVCE ACM Student Chapter"
+        desc : "The members of the VVCE ACM Student Chapter were inducted on the working of the chapter and the events that are planned for their benefits."
+        date : "13th November, 2021"
+        poster : "./Assets/induction.jpg"
+    }
+    {
+        title : "Collaboration announcement"
+        desc : "We are happy to announce that we've signed a collaboration agreement with UB ACM Student Chapter to bring collaborative events to the members."
+        date : "9th November, 2021"
+        poster : "./Assets/collab.jpg"
+    }
+
+    {
+        title : "AICDMB 2022"
+        desc : "Farewell ceremony to the senior officers and handover of posts to the new officers"
+        date : "8th November, 2021"
+        poster : "./Assets/aicdmb22.png"
+    }
+    {
+        title : "Workshop: Intro to Web Development"
+        desc : "A hands-on workshop focusing on fundamentals of Web Development including HTML, CSS and javascript ES6"
+        date : "22nd September, 2021"
+        poster : "./Assets/web_dev.jpg"
+    }
+    {
+        title : "TECHNIQUIZ"
+        desc : "A quiz based on technical general knowledge was conducted"
+        date : "31st August, 2021"
+        poster : "./Assets/techniquiz.jpg"
+    }
+    {
+        title : "VVCE ACM Greet and Meet - 2021"
+        desc : "Farewell ceremony to the senior officers and handover of posts to the new officers"
+        date : "3rd August, 2021"
+        poster : "./Assets/greetnmeet21.jpg"
+    }
+    {
+        title : "ACM DSP: Ricardo Baeza-Yates"
+        desc : "Big Data or Right Data? Opportunities and Challenges"
+        date : "2nd June, 2021"
+        poster : "./Assets/bd_acm_dsp_thumb.svg"
+    }
+    {
+        title : "ICRTCS - 21"
+        desc : "AICTE Sponsored Second International Colloquium on Recent Trends in Computer Science - 2021"
+        date : "25th May, 2021"
+        poster : "./Assets/ICRTCS_thumb.jpg"
+    }
+    {
+        title : "Recieved grant from AICTE under SPICES Scheme"
+        desc : "We are proud to announce that our chapter has recieved a grant of <span>&#8377;</span>1,00,000 from AICTE..."
+        date : "5th Mar, 2021"
+        poster : "./Assets/aicte_grant_thumb.jpg"
+    }
+    {
+        title : "ACM DSP: Dr. Ashutosh Dutta"
+        desc : "Security in 5G Networks - Challenges and Opportunities"
+        date : "31 Dec, 2020"
+        poster : "./Assets/ADSP_.jpg"
+    }
+    {
+        title : "Update: Collaboration with University of Houston, Downtown"
+        desc : "Very happy and proud to inform that Prof. Lin, University of Houston, USA agreed to collaborate with our Institution. Looking forward for the great eductional journey."
+        date : "28th Jul, 2020"
+        poster : "./Assets/udhvvce-100.jpg"
+    }
+    {
+        title : "Event: International Colloquium on Recent Trends in Computer Science"
+        desc : "International Colloquium aims at bringing together Researchers, Engineers, Scientists and Industrialists from all over the world to share and discuss advances and challenges in digital transformation. ICRTCS-2020 mainly focuses on IoT & Artificial Intelligence"
+        date : "20th-24th Jul, 2020"
+        poster : "./Assets/events/icrtcs.png"
+    }
+    {
+        title : "Event: Bug Fix"
+        desc : "The first of its kind event. No coding from scratch. Fix the program with your superior code."
+        date : "10th Jul, 2020"
+        poster : "./Assets/bugfix.jpg"
+    }
+    {
+        title : "Cyber System Security Webinar"
+        desc : "Webinar on Cyber System Security by Dr. Sandeep Shukla (IIT Kanpur)"
+        date : "18th May, 2020"
+        poster : "./Assets/img/event/webinar_cybersecurity.jpeg"
+    }
+    {
+        title : "Event: Web-A-Thon"
+        desc : "Design a website using HTML/CSS/JS and compete with other participants!"
+        date : "25th Mar, 2020"
+        poster : "./Assets/webathon.jpg"
+    }
+    {
+        title : "AICDMB 2020"
+        desc : "Annual International Conference on Data Science, Machine Learning and Blockchain Technology (AICDMB-2020)"
+        date : "12th Feb, 2020"
+        poster : "./Assets/img/event/aicdmb.jpg"
+    }
+        {
+        title : "Hour of Code 2019-20"
+        desc : "Annual Hour of Code was held."
+        date : "21st Dec, 2020"
+        poster : "./Assets/img/event/hoc32019.jpeg"
+    }
+    {
+        title : "Workshop: Intro to CyberSecurity"
+        desc : "Two day workshop on Cybersecurity Introduction to cybersecurity Introduction to kali linux Malware Analysis Basics of Pen testing"
+        date : "19th Sept, 2019"
+        poster : "./Assets/img/event/cybersecurity_workshop.jpg"
+    }
+    {
+        title : "Workshop: Paper Writing Skills"
+        desc : "A Workshop on paper writing skills was held."
+        date : "7th Sept, 2019"
+        poster : "./Assets/img/event/paper_writing_wing.jpeg"
+    }
+    {
+        title : "ACM DSP: Dr. Ramasuri Narayanam"
+        desc : "Design and Analysis of Graphical Password. Unsupervised Learning in Data Mining."
+        date : "17th Aug, 2019"
+        poster : "./Assets/img/event/Acm_dsp_ramasuri.jpg"
+    }
+    {
+        title : "ACM Meet and Greet Program"
+        desc : "The Old officers are felicitated with a token of appreciation and new officers are elected by giving the appointment letter by Dr. Ravi Kumar V, HOD-CSE addressed the gathering by telling the importance ofACMChapter."
+        date : "8th May, 2019"
+        poster : "./Assets/img/event/6.png"
+    }
+    {
+        title : "Workshop: Research"
+        desc : "Essential Research Identifiers and Benefits of Reviewing Manuscripts for Quality Journals. & Routing Protocols in Underwater Sensor Networks: Challenges and Future Directions."
+        date : "11th Mar, 2019"
+        poster : "./Assets/img/event/3.jpg"
+    }
+    {
+        title : "ACM DSP: Dr.G.R.Sinha"
+        desc : '''ACM Student Chapter Inauguration and "Developing Employability skills using ICT Tools" by ACM DSP, Talk on Study of Cognitive Ability of Human Brain'''
+        date : "24th Oct, 2019"
+        poster : "./Assets/img/event/2.jpg"
+    }
+    {
+        title : "Workshop: Design Thinking"
+        desc : '''Workshop on "Design Thinking" was done on October 9, 2018 by Subbu Iyer'''
+        date : "9th Oct, 2019"
+        poster : "./Assets/img/event/1.jpg"
+    }
+    {
+        title : "Hour of Code 2018-19"
+        desc : "Annual Hour of Code was held"
+        date : "11th Mar, 2019"
+        poster : "./Assets/img/event/4.jpeg"
+    }
+    
+    {
+        title : "Industrial Visit"
+        desc : "Industrial visit to ISRO Master Control Facility (MCF) on October 1, 2018"
+        date : "1st Oct, 2019"
+        poster : "./Assets/img/event/5.jpg" 
+    }
+]
